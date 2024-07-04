fbfa310ee0215515713e62f0cac128f9a9da0fef
AntonioDattolo <antonio19794.a@gmail.com>
2024-07-04 20:00:12 +0200

    AGGIUNTA SEZIONE DEL FORM

--- app/Http/Controllers/ResourceComicController.php
+++ app/Http/Controllers/ResourceComicController.php
@@ -19,7 +19,7 @@ class ResourceComicController extends Controller
      */
     public function create()
     {
-        //
+        return view("create");
     }
 
     /**
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -9,7 +9,7 @@
                         <a class="nav-link active text-white" aria-current="page" href="{{route("home")}}">HOME</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link text-white" href="#">ADD COMIC</a>
+                        <a class="nav-link text-white" href="index/create">ADD COMIC</a>
                     </li>
                 </ul>
             </div>
