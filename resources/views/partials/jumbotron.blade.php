6e7ba8cfa58d6a975b8f4fee3d52645ff376bb15
AntonioDattolo <antonio19794.a@gmail.com>
2024-07-06 14:09:38 +0200

    Update Preview

--- resources/views/partials/jumbotron.blade.php
+++ resources/views/partials/jumbotron.blade.php
@@ -1,5 +1,6 @@
-<section class="container-fluid p-0">
-    <img id="jumbo" class="w-100" src="{{URL::to('img/jumbotron.jpg')}}" alt="">
+<section  class="container-fluid p-0">
+    <div id="jumbo"></div>
+    {{-- <img id="jumbo" class="w-100" src="{{URL::to('img/jumbotron.jpg')}}" alt=""> --}}
         {{-- Slider --}}
         @include('partials.slider')
 </section>
